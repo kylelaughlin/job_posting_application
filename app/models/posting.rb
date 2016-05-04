d6891ed36a6f9eaf6aa2ccca3964ca33c3d63ae8
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-04 14:40:47 -0500

    added in validations

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,2 +1,6 @@
 class Posting < ActiveRecord::Base
+  validates :company, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
 end
