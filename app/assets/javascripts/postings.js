e247e07548da0054d9bd2ac12ba1dd0a12e08046
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-04 22:53:03 -0500

    add js to animate hiding of flash; update errors to be red

--- app/assets/javascripts/postings.js
+++ app/assets/javascripts/postings.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+//Show flashes for 4 seconds then animate slide up to hide
+$(function() {
+  $("#flash").slideDown(400).delay(4000).slideUp(400);
+});
